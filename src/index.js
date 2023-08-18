af39248bddd9fba89d63775a92cea8fad40c4c89
Mikele Corleone <sellmar38@gmail.com>
2023-08-18 18:07:12 +1000

    v1.3.2

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ app.use(notFound);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
